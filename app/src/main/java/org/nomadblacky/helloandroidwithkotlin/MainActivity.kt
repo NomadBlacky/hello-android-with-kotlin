740e50f8574b2b3c1dd43dae1da5f8c507aab5fc
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-04-24 19:58:58 +0900

    Activity

--- app/src/main/java/org/nomadblacky/helloandroidwithkotlin/MainActivity.kt
+++ app/src/main/java/org/nomadblacky/helloandroidwithkotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package org.nomadblacky.helloandroidwithkotlin
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        val button = Button(this)
+        button.text = "Hello!"
+        button.setOnClickListener { button.text = "Android!" }
+        setContentView(button)
     }
+
 }
